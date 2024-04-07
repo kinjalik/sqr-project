6ad16fc94ae6af1d280182b7d3bcad930dabb4cd
adarika <ADari.Ka@yandex.ru>
2024-04-07 16:06:23 +0300

    ref+add: project organization

--- scripts/dev_run.sh
+++ scripts/dev_run.sh
@@ -3,11 +3,12 @@ set -e
 
 BASEDIR=$(dirname "$0")
 
-# Streamlit is cursed!
-mkdir -p ~/.streamlit/
-echo "[general]"  > ~/.streamlit/credentials.toml
-echo "email = \"\""  >> ~/.streamlit/credentials.toml
+# # Streamlit is cursed!
+# mkdir -p ~/.streamlit/
+# echo "[general]"  > ~/.streamlit/credentials.toml
+# echo "email = \"\""  >> ~/.streamlit/credentials.toml
 
-pushd "$BASEDIR/../src/app"
-    bash ./run.sh
+pushd "$BASEDIR/../src/"
+    # bash ./run.sh
+    # TODO
 popd
